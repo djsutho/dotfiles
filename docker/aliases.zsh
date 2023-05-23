80b5edff26ea17557459200c800e1862df35178c
David Sutherland <dasutherland@expediagroup.com>
2023-05-23 12:43:44 +1000

    docker and java aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,6 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
 alias docker_last_built="docker images --format '{{.ID}} {{.CreatedAt}} {{.Repository}} {{.Tag}}' | sort -rk 2 | awk 'NR==1{print \$1}'"
+alias docker_running_image='docker ps -q | head -n1'
+alias docker_running_command='docker exec -it "$(docker_running_image)"'
+alias docker_clean='docker system prune --all --volumes && docker run --privileged --pid=host docker/desktop-reclaim-space -v'
--- java/aliases.zsh
+++ java/aliases.zsh
@@ -19,3 +19,13 @@ alias mvnqi='mvn -T 2C install \
   -DskipITs \
   -Dmaven.javadoc.skip=true \
   --no-snapshot-updates'
+alias mvnqsb='mvn -T 2C spring-boot:run \
+  -Dmaven.test.skip=true \
+  -DskipTests \
+  -Dfindbugs.skip=true \
+  -Dpmd.skip=true \
+  -Dcheckstyle.skip \
+  -Dspotbugs.skip=true \
+  -DskipITs \
+  -Dmaven.javadoc.skip=true \
+  --no-snapshot-updates'
