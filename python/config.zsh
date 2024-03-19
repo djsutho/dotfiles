173e9f852593827fa65d069202787e7582052c31
David Sutherland <dasutherland@expediagroup.com>
2024-03-19 15:13:08 +1000

    fix more things

--- python/config.zsh
+++ python/config.zsh
@@ -1,5 +1,4 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/dev
 export VIRTUALENVWRAPPER_PYTHON=`which python3`
-export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+export VIRTUALENVWRAPPER_SCRIPT=`which virtualenvwrapper.sh`
